c4a2fff92b72ee1a25df5046e66d18559c2a4ca2
trinhntd <diemtrinhvlm97@gmail.com>
2024-01-13 11:28:51 +0700

    feat: change substring to 1000 for sql log + img not found

--- packages/backend/src/postgres.ts
+++ packages/backend/src/postgres.ts
@@ -96,7 +96,7 @@ class MyCustomLogger implements Logger {
 
 	@bindThis
 	public logQuery(query: string, parameters?: any[]) {
-		sqlLogger.info(this.highlight(query).substring(0, 100));
+		sqlLogger.info(this.highlight(query).substring(0, 1000));
 	}
 
 	@bindThis
